d0046104b86ea93b2fdb1432362424dc0dee0141
walten <wolfgang.waltenberger@gmail.com>
2020-10-14 19:53:02 +0200

    colors

--- toyWalk/makeMovie.py
+++ toyWalk/makeMovie.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import warnings
+warnings.filterwarnings("ignore")
 
 import sys,os,copy, pickle, glob, subprocess
 import numpy as np
@@ -22,11 +24,17 @@ sns.set_context('paper', font_scale=2.0)
 # sns.set_palette(sns.color_palette("Paired"))
 sns.set_palette(sns.color_palette("deep"))
 
+cmd = 'rm -f walk*.png'
+subprocess.getoutput( cmd )
+
 #Set colors:
 namer = SParticleNames ( susy = False )
 #Replace default colors:
 colorPalette = 'deep' #Set color palette for particles, if None use default
-colorDict = dict(zip(namer.names.keys(),sns.color_palette(palette=colorPalette,n_colors=len(namer.names))))
+allpids = [ 1000001, 1000002, 1000003, 1000004, 1000005, 1000006,          
+2000005, 2000006, 1000011, 1000012, 1000013, 1000014, 1000015,          
+1000016, 1000021, 1000022, 1000023, 1000025, 1000024, 1000037 ]
+colorDict = dict(zip( allpids,sns.color_palette(palette=colorPalette,n_colors=len(namer.names))))
 
 f=open("history.list","rt")
 txt=f.read()
@@ -48,7 +56,12 @@ nparticles = np.array([len(p["masses"]) for p in modelList])
 Kvalues = np.array([p["K"] if (p["K"] and p["K"] > 0) else 0.0 for p in modelList])
 Zvalues = np.array([p["Z"] if (p["Z"] and p["Z"] > 0) else 0.0 for p in modelList])
 masses = dict([[pid,[]] for pid in particles])
+Ks=[]
 for p in modelList:
+    K=p["K"]
+    if K == None:
+        K=0.
+    Ks.append(K)
     for pid in masses:
         if pid in p["masses"]:
             masses[pid].append(float(p["masses"][pid]))
@@ -63,7 +76,11 @@ dataDict.update(masses)
 df = pd.DataFrame(dataDict)
 fig = plt.figure(figsize=(10, 6))
 nsteps = 1
-for firststep in range ( 2000 ):
+maxstep = 500
+#maxstep = 10
+#maxstep = 200
+
+for firststep in range ( maxstep ):
     laststep=firststep+20
 
     nvalues = {}
@@ -74,21 +91,29 @@ for firststep in range ( 2000 ):
                     nvalues[k]+=1
     pids = sorted(masses.keys(), key = lambda pid: nvalues[pid] )
     #pids = sorted(masses.keys(), key = lambda pid: np.sum(np.where(masses[pid][::nsteps] <= 0.)))
+    ctentries=0
     for pid in pids:
         if max(masses[pid][firststep:laststep:nsteps]) <= 0.0: continue
+        ctentries+=1
         data = df[firststep:laststep:nsteps]
+        tName = r'$%s$' % namer.texName(pid)
+        c = colorDict[pid]
+        #if ctentries>9:
+        #        tName=""
         sns.scatterplot(x=data['step'],y=data[pid], size=data['K'], sizes = (80,400),
-                        label=r'$%s$' %(namer.texName(pid)), legend=False)
+                        label= tName , color=c, legend=False)
         m = np.where(masses[pid] > 0, masses[pid],np.nan) #Fix for avoid plotting to negative values
-        plt.plot(df['step'][firststep:laststep:nsteps],m[firststep:laststep:nsteps],'-',linewidth=2)
+        plt.plot(df['step'][firststep:laststep:nsteps],m[firststep:laststep:nsteps],'-',linewidth=2, color = c)
 
     plt.ylim(0.,2500.0)
+    plt.title ( 'K=%.2f' % Ks[firststep], loc="left" )
     plt.xlabel('step', fontsize=23)
     plt.ylabel('Mass [GeV]', fontsize=23)
-    plt.xticks(df['step'][firststep:laststep:2*nsteps])
+    plt.xticks(df['step'][firststep:laststep:1*nsteps])
     # plt.xlim(-5,198)
     plt.grid(axis='x') 
-    plt.legend(loc=(0.8,0.75),framealpha=1.0,ncol=3,labelspacing=0.1,
+    plt.legend(loc=(.8,.75),bbox_to_anchor=(0.6,0.5,.2,.25), 
+               framealpha=1.0,ncol=3,labelspacing=0.1,
                handlelength=0.4,handletextpad=0.35,markerscale=0.8,columnspacing=1.0)
     # plt.tight_layout()
     plt.savefig('walk%d.png' % firststep )
