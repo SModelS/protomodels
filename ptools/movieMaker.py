05f06d7882de6c13abf5d34c8bd01208a459d7c7
walten <wolfgang.waltenberger@gmail.com>
2020-10-25 12:01:56 +0100

    smoother movies

--- ptools/movieMaker.py
+++ ptools/movieMaker.py
@@ -49,6 +49,8 @@ argparser.add_argument ( '-t', '--timestamp',
         action="store_true" )
 args = argparser.parse_args()
 
+    
+intermediateSteps = True ## do 10 rendering steps per one random walk step
 
 if not args.dont_clean:
     cmd = 'rm -f walk*.png'
@@ -167,130 +169,149 @@ print ( "[movieMaker] setting maxstep to %d" % maxstep )
 style = "Simple, tail_width=0.5, head_width=4, head_length=8"
 kw = dict(arrowstyle=style, color="k")
 
+
 for firststep in range ( args.start, maxstep ):
-    fig, (ax1, ax2) = plt.subplots( ncols=2, sharey=True, gridspec_kw={'width_ratios': [1, 10]} )
     if firststep % 10 == 0:
         print ( "step %d" % firststep )
-    laststep=firststep+20
+    lastingHS = 0 ## the "hiscore!" label should last a bit
+    alloffs = [ 0. ]
+    if intermediateSteps:
+        alloffs = np.arange(0,1.,.1)
 
-    nvalues = {}
-    for k,v in masses.items():
-        nvalues[k]=0
-        for i in v:
-            if float(i)>0:
-                    nvalues[k]+=1
-    pids = sorted(masses.keys(), key = lambda pid: nvalues[pid] )
-    #pids = sorted(masses.keys(), key = lambda pid: np.sum(np.where(masses[pid][::nsteps] <= 0.)))
-    ctentries=0
-    K=Ks[firststep+currentstep]
-    #if K < 0.:
-    #    K = 0.
-    plt.text ( -3+firststep-nstart, 1250, "hiscore", rotation=90., c="pink", alpha=.5, size=30,
-            horizontalalignment='center', verticalalignment='center', zorder=5 )
-    if K > maxK:
-        maxK = K
-        stepatmax = firststep+currentstep
-        plt.text ( .5+firststep-nstart, 2200, "hiscore!", c="red", size=30, clip_on=False )
-    # arrow = plt.arrow ( currentstep, -50, -currentstep-2, 0, width=20, clip_on=False, transform=ax2.transData, color="black" )
-    # arrow = patches.FancyArrowPatch((.1, .5), (.1, .5), clip_on=False,
-    #arrow = patches.FancyArrowPatch((firststep+currentstep-5-nstart, firststep-nstart), ( 50, 50), 
-    #    clip_on=False, connectionstyle="arc3,rad=.1", **kw, transform=ax2.transData )
-    # plt.gca().add_patch ( arrow )
+    for offs in alloffs:
+        fig, (ax1, ax2) = plt.subplots( ncols=2, sharey=True, gridspec_kw={'width_ratios': [1, 10]} )
+        laststep=firststep+20
 
-    for pid in pids:
-        if max(masses[pid][firststep:laststep:nsteps]) <= 0.0: continue
-        ctentries+=1
-        data = df[firststep:laststep:nsteps]
-        datamax = df[stepatmax:stepatmax+1]
-        datacur = df[firststep+currentstep:firststep+currentstep+1]
-        tName = r'$%s$' % namer.texName(pid)
-        c = colorDict[pid]
-        #if ctentries>9:
-        #        tName=""
-        m = np.where(masses[pid] > 0, masses[pid],np.nan) #Fix for avoid plotting to negative values
-        plt.plot(df['step'][firststep:laststep:nsteps],m[firststep:laststep:nsteps],'-',linewidth=2, color = c, alpha=.5 )
-        sizes = []
-        def getSize ( k ):
-            if k < 0.: k = 0.
-            if k in [ None, float("nan"), float("inf") ]:
-                k = 0.
-            return (1+.6*k)*80
-        for s in data["K"]:
-            sizes.append( getSize ( s ) )
-        sns.scatterplot(x=data['step'],y=data[pid], s=sizes, sizes = (80,400),
-                        label= tName, color=c, legend=False, ax=ax2, alpha=.5 )
-        # s= (1+1.25*maxK)*80. ## no idea why
-        smax= getSize ( maxK ) ## no idea why
-        s = getSize ( K ) ## no idea why
-        sns.scatterplot(x=datacur['step'],y=datacur[pid], s=1.2*s+40, sizes = (80,400),
-                        label= "", color="black", legend=False, ax=ax2, edgecolor="none",
-                        linewidth=0 )
-        sns.scatterplot(x=datacur['step'],y=datacur[pid], s=s, sizes = (80,400),
-                        label= "", linewidth=0, edgecolor="none", color=c, 
-                        legend=False, ax=ax2 )
-        sns.scatterplot(x=datamax['step'],y=datamax[pid], s=smax, sizes = (80,400),
-                        label= tName , color=c, legend=False, ax=ax1, zorder=10)
-    plt.ylim(0.,2500.0)
-    #plt.title ( '$K_{max}=%.1f, K_{current}=%.1f$' % ( maxK, K ), loc="left" )
-    title='$K_{current}=%.1f\;\,$   $\;$ $\;\;\;\;\;\,$ $\;$ $\;$ $\;$ ' % ( K )
-    if K < -50.:
-        title=""
-    ax2.set_title ( title, fontsize=18, pad=15., horizontalalignment="center" )
-    ax1title = '$K_{max}=%.1f$' % ( maxK )
-    if maxK < -50.:
-        ax1title = ""
-    ax1.set_title ( ax1title, fontsize=18, pad=15. )
-    ax2.set_xlabel('step', fontsize=20, labelpad=-3. )
-    ax1.set_xlabel('', fontsize=21 )
-    ax1.set_xticks([])
-    plt.ylabel('Mass [GeV]', fontsize=21 )
-    ax2.set_ylabel('Mass [GeV]', fontsize=21 )
-    ax1.set_ylabel('Mass [GeV]', fontsize=21, labelpad=-5. )
-    dstep = 10 ## 2
-    #plt.xticks(df['step'][firststep:laststep:2*nsteps])
-    nextstep = math.ceil((firststep+1)/dstep)*dstep-1
-    ticks=[]
-    for t in df['step'][nextstep:laststep:dstep]:
-        if t < 1:
-            continue
-            # t = 0
-        ticks.append ( t )
-    plt.xticks( ticks )
-    # plt.xticks(df['step'][nextstep:laststep:dstep])
-    # plt.xlim(-5,198)
-    plt.grid(axis='x')
-    ac=actions[firststep+currentstep]
-    lac = len(ac)
-    while len(ac)<3:
-        ac.append ( "" )
-    if lac>0:
-        ss = math.ceil ( len(ac)/3 )
-        txt="\n".join( ac[:3] )
-        # txt="\n".join([ x.replace("->","$\\rightarrow$") for x in ac[::ss] ])
-        plt.text ( -8+firststep-nstart, -320, txt, c="gray", size=7 )
-    bc=bcs[firststep+currentstep]
-    lbc=len(bc)
-    #while len(bc)<6:
-    #    bc.append( "" )
-    if lbc>0:
-        ss = math.ceil ( len(bc)/6 )
-        txt="\n".join([ x[:x.find(":")] for x in bc[::ss] ] )
-        bbox = None
-        bbox=dict(boxstyle="round", ec=(1., 0.5, 0.5), fc=(1., 0.8, 0.8),)
-        plt.text ( 18+firststep-nstart, 10, txt, size=10, horizontalalignment="right", verticalalignment="bottom", c="gray", bbox=bbox, rotation=10. )
+        nvalues = {}
+        for k,v in masses.items():
+            nvalues[k]=0
+            for i in v:
+                if float(i)>0:
+                        nvalues[k]+=1
+        pids = sorted(masses.keys(), key = lambda pid: nvalues[pid] )
+        #pids = sorted(masses.keys(), key = lambda pid: np.sum(np.where(masses[pid][::nsteps] <= 0.)))
+        ctentries=0
+        K=Ks[firststep+currentstep]
+        #if K < 0.:
+        #    K = 0.
+        plt.text ( -3+firststep-nstart+offs, 1250, "hiscore", rotation=90., c="pink", alpha=.5, size=30,
+                horizontalalignment='center', verticalalignment='center', zorder=5 )
+        if K > maxK:
+            lastingHS = 5 ## keep it for 5 frames
+            maxK = K
+        if lastingHS>0:
+            maxK = K
+            stepatmax = firststep+currentstep
+            plt.text ( .5+firststep-nstart+offs, 2200, "hiscore!", c="red", size=30, clip_on=False )
+            lastingHS-=1 ## count down to zero
+        # arrow = plt.arrow ( currentstep, -50, -currentstep-2, 0, width=20, clip_on=False, transform=ax2.transData, color="black" )
+        # arrow = patches.FancyArrowPatch((.1, .5), (.1, .5), clip_on=False,
+        #arrow = patches.FancyArrowPatch((firststep+currentstep-5-nstart, firststep-nstart), ( 50, 50), 
+        #    clip_on=False, connectionstyle="arc3,rad=.1", **kw, transform=ax2.transData )
+        # plt.gca().add_patch ( arrow )
 
-    plt.legend(loc=(.6,.7),# bbox_to_anchor=(0.6,0.5,.2,.25),
-               framealpha=1.0,ncol=3,labelspacing=0.1,
-               handlelength=0.4,handletextpad=0.35,markerscale=0.8,columnspacing=1.0)
-    # plt.tight_layout()
-    if args.timestamp:
-        plt.text ( 15+firststep-nstart, -280, time.asctime(), size=8, alpha=.5, c="gray" )
-    step = firststep + 1 ## make all one-indexed, ok?
-    plt.savefig('walk%.3d.png' % step, dpi=200 )
-    # plt.show()
-    plt.clf()
+        for pid in pids:
+            if max(masses[pid][firststep:laststep:nsteps]) <= 0.0: continue
+            ctentries+=1
+            data = df[firststep:laststep:nsteps]
+            datamax = df[stepatmax:stepatmax+1]
+            datacur = df[firststep+currentstep:firststep+currentstep+1]
+            tName = r'$%s$' % namer.texName(pid)
+            c = colorDict[pid]
+            #if ctentries>9:
+            #        tName=""
+            m = np.where(masses[pid] > 0, masses[pid],np.nan) #Fix for avoid plotting to negative values
+            ## the lines
+            plt.plot(df['step'][firststep:laststep+2:nsteps],m[firststep:laststep+2:nsteps],'-',linewidth=2, color = c, alpha=.5 )
+            sizes = []
+            def getSize ( k ):
+                if k < 0.: k = 0.
+                if k in [ None, float("nan"), float("inf") ]:
+                    k = 0.
+                return (1+.6*k)*80
+            for s in data["K"]:
+                sizes.append( getSize ( s ) )
+            sns.scatterplot(x=data['step'],y=data[pid], s=sizes, sizes = (80,400),
+                            label= tName, color=c, legend=False, ax=ax2, alpha=.5 )
+            # s= (1+1.25*maxK)*80. ## no idea why
+            smax= getSize ( maxK ) ## no idea why
+            s = getSize ( K ) ## no idea why
+            sns.scatterplot(x=datacur['step'],y=datacur[pid], s=1.2*s+40, sizes = (80,400),
+                            label= "", color="black", legend=False, ax=ax2, edgecolor="none",
+                            linewidth=0 )
+            sns.scatterplot(x=datacur['step'],y=datacur[pid], s=s, sizes = (80,400),
+                            label= "", linewidth=0, edgecolor="none", color=c, 
+                            legend=False, ax=ax2 )
+            sns.scatterplot(x=datamax['step'],y=datamax[pid], s=smax, sizes = (80,400),
+                            label= tName , color=c, legend=False, ax=ax1, zorder=10)
+        plt.ylim(0.,2500.0)
+        #plt.title ( '$K_{max}=%.1f, K_{current}=%.1f$' % ( maxK, K ), loc="left" )
+        title='$K_{current}=%.1f\;\,$   $\;$ $\;\;\;\;\;\,$ $\;$ $\;$ $\;$ ' % ( K )
+        if K < -50.:
+            title=""
+        ax2.set_title ( title, fontsize=18, pad=15., horizontalalignment="center" )
+        ax1title = '$K_{max}=%.1f$' % ( maxK )
+        if maxK < -50.:
+            ax1title = ""
+        ax1.set_title ( ax1title, fontsize=18, pad=15. )
+        ax2.set_xlabel('step', fontsize=20, labelpad=-3. )
+        ax1.set_xlabel('', fontsize=21 )
+        ax1.set_xticks([])
+        plt.ylabel('Mass [GeV]', fontsize=21 )
+        ax2.set_ylabel('Mass [GeV]', fontsize=21 )
+        ax1.set_ylabel('Mass [GeV]', fontsize=21, labelpad=-5. )
+        dstep = 10 ## 2
+        #plt.xticks(df['step'][firststep:laststep:2*nsteps])
+        nextstep = math.ceil((firststep+1)/dstep)*dstep-1
+        ticks=[]
+        for t in df['step'][nextstep:laststep:dstep]:
+            if t < 1:
+                continue
+                # t = 0
+            ticks.append ( t )
+        plt.xticks( ticks )
+        # plt.xticks(df['step'][nextstep:laststep:dstep])
+        # plt.xlim(-5,198)
+        plt.grid(axis='x')
+        ac=actions[firststep+currentstep]
+        lac = len(ac)
+        while len(ac)<3:
+            ac.append ( "" )
+        if lac>0:
+            ss = math.ceil ( len(ac)/3 )
+            txt="\n".join( ac[:3] )
+            # txt="\n".join([ x.replace("->","$\\rightarrow$") for x in ac[::ss] ])
+            plt.text ( -8+firststep-nstart+offs, -320, txt, c="gray", size=7 )
+        bc=bcs[firststep+currentstep]
+        lbc=len(bc)
+        #while len(bc)<6:
+        #    bc.append( "" )
+        if lbc>0:
+            ss = math.ceil ( len(bc)/6 )
+            txt="\n".join([ x[:x.find(":")] for x in bc[::ss] ] )
+            bbox = None
+            bbox=dict(boxstyle="round", ec=(1., 0.5, 0.5), fc=(1., 0.8, 0.8),)
+            plt.text ( 18+firststep-nstart, 10, txt, size=10, horizontalalignment="right", verticalalignment="bottom", c="gray", bbox=bbox, rotation=10. )
 
-#cmd = 'ffmpeg -y -i "walk%3d.png" -filter:v "setpts=3.0*PTS, minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=25\'" walk.mp4'
+        plt.legend(loc=(.6,.7),# bbox_to_anchor=(0.6,0.5,.2,.25),
+                   framealpha=1.0,ncol=3,labelspacing=0.1,
+                   handlelength=0.4,handletextpad=0.35,markerscale=0.8,columnspacing=1.0)
+        # plt.tight_layout()
+        if args.timestamp:
+            plt.text ( 15+firststep-nstart, -280, time.asctime(), size=8, alpha=.5, c="gray" )
+        step = firststep + 1 ## make all one-indexed, ok?
+        off1 = firststep-nstart+.05+offs
+        ax2.set_xlim ( off1, off1 + 21 )
+        if intermediateSteps:
+            plt.savefig('walk%.3d%d.png' % (firststep, int(offs*10)), dpi=200 )
+        else:
+            plt.savefig('walk%.3d.png' % step, dpi=200 )
+        # plt.show()
+        plt.clf()
+
 cmd = 'ffmpeg -y -i "walk%3d.png" -filter:v "setpts=6.0*PTS" walk.mp4'
+if intermediateSteps:
+    cmd = 'ffmpeg -y -i "walk%4d.png" walk.webm'
+#cmd = 'ffmpeg -y -i "walk%3d.png" -filter:v "setpts=3.0*PTS, minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=25\'" walk.mp4'
 # cmd = 'ffmpeg -y -i "walk%3d.png" walk.mp4'
 subprocess.getoutput ( cmd )
