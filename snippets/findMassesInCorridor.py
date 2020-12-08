aed5d4bf5cfa9a675dca8e74fcf43398d21bfc2c
wolfgang waltenberger <wolfgang.waltenberger@gmail.com>
2020-12-08 17:00:42 +0100

    debug tool

---
+++ snippets/findMassesInCorridor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+fname = "/scratch-cbe/users/wolfgan.waltenberger/rundir.fudged1/history34.list" 
+with open ( fname, "rt" ) as h:
+    txt = h.read()
+    if txt[-2] == ",":
+        txt+="]\n"
+    L = eval ( txt )
+
+print ( len(L), "steps." )
+
+for model in L:
+    if not 1000006 in model["masses"]:
+        continue
+    step = model["step"]
+    dm = model["masses"][1000006] - model["masses"][1000022] 
+    inCorridor = 150 < dm < 200
+    if inCorridor:
+        print ( "step", step , "dm", dm )
+
