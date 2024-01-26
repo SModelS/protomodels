06185ea307e219a45fddd056b062e6a1215ad5a6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-01-26 15:44:03 +0100

    code to merge two models

---
+++ snippets/mergeTwoModels.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import time
+from ptools.hiscoreTools import mergeTwoModels
+model = mergeTwoModels ( "pmodel_ewkino.py", "pmodel_hadrons.py" )
+print ( "model", model )
+f=open("merged.py","wt")
+f.write ( "# this model is due to a merge of the ewkino and the hadrons models\n" )
+f.write ( f"# created {time.asctime()}\n" )
+f.write ( str(model)+"\n" )
+f.close()
