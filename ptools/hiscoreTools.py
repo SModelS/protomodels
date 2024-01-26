02ca4a4e81ff064fd2a341c5a42ddfdf89c9d19d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-01-26 15:44:03 +0100

    code to merge two models

--- ptools/hiscoreTools.py
+++ ptools/hiscoreTools.py
@@ -32,8 +32,7 @@ def storeList ( protomodels, savefile ):
     """ store the best protomodels in another hiscore file """
     h = Hiscore ( 0, True, savefile, backup=True, hiscores = protomodels )
     h.hiscores = protomodels
-    print ( "[hiscore] saving %d protomodels to %s" % \
-            ( count(protomodels), savefile ) )
+    print ( f"[hiscore] saving {count(protomodels)} protomodels to {savefile}" )
     if savefile.endswith ( ".pcl" ) or savefile.endswith( ".hi" ):
         h.writeListToPickle ( check=False )
         if "states" in savefile: ## do both for the states
@@ -51,9 +50,9 @@ def discussBest ( protomodel, detailed ):
     print ( "Current      best K=%.3f, Z=%.3f, p=%.2g [%d/%d particles, %d predictions] (walker #%d)" % \
             ( protomodel.K, protomodel.Z, p, len(protomodel.unFrozenParticles()),len(protomodel.masses.keys()),len(protomodel.bestCombo), protomodel.walkerid ) )
     if detailed:
-        print ( "Solution was found in step #%d" % protomodel.step )
+        print ( f"Solution was found in step #{protomodel.step}" )
         for i in protomodel.bestCombo:
-            print ( "  prediction in best combo: %s (%s)" % ( i.analysisId(), i.dataType() ) )
+            print ( f"  prediction in best combo: {i.analysisId()} ({i.dataType})" )
 
 def printProtoModels ( protomodels, detailed, nmax=10 ):
     names = { 0: "highest", 1: "second", 2: "third" }
@@ -73,7 +72,7 @@ def printProtoModels ( protomodels, detailed, nmax=10 ):
 def pprintEvs ( protomodel ):
     """ pretty print number of events """
     if protomodel.nevents > 1000:
-        return "%dK evts" % ( protomodel.nevents/1000 )
+        return f"{protomodel.nevents/1000}K evts"
     return str(protomodel.nevents)+ " evts"
 
 def hiscoreHiNeedsUpdate ( dictfile : str = "hiscores.dict", 
@@ -192,8 +191,7 @@ def updateHiscoreHi ( args : Namespace ) -> Dict:
     if sin == None:
         sin = "H*.hi"
     pevs = pprintEvs ( protomodels[0] )
-    print ( "[hiscore] hiscore from %s[%d] is at K=%.3f, Z=%.3f (%s)" % \
-            ( sin, protomodels[0].walkerid, protomodels[0].K, protomodels[0].Z, pevs ) )
+    print ( f"[hiscore] hiscore from {sin}[{protomodels[0].walkerid}] is at K={protomodels[0].K:.3f}, Z={protomodels[0].Z:.3f} ({pevs})" )
 
     if type(args.outfile)==str and (".pcl" in args.outfile or ".hi" in args.outfile ):
         if not hasattr ( protomodels[0], "analysisContributions" ):
@@ -238,4 +236,39 @@ def updateHiscoreHi ( args : Namespace ) -> Dict:
         ret["step"]=protomodels[0].step
         ret["model"]=protomodels[0]
         return ret
+    return ret
+
+def mergeTwoModels ( model1 : Dict, model2: Dict ) -> Union[None,Dict]:
+    """ merge two models, add all particles from both models.
+    If a particle appears in both models, its mass will be the average 
+    of the two, etc.
+
+    :returns: merged model
+    """
+    if not os.path.exists ( model1 ):
+        print ( f"[hiscoreTools] {model1} does not exist" )
+        return None
+    if not os.path.exists ( model2 ):
+        print ( f"[hiscoreTools] {model2} does not exist" )
+        return None
+    f=open ( model1, "rt" )
+    txt=f.read()
+    f.close()
+    dict1 = eval(txt)
+    f=open ( model2, "rt" )
+    txt=f.read()
+    f.close()
+    dict2 = eval(txt)
+    import copy
+    ret = copy.deepcopy(dict1)
+    for pid,m in dict2["masses"].items():
+        if not pid in ret["masses"]:
+            # ok, we just copy <pid> from dict2
+            ret["masses"][pid]=m
+            ret["decays"][pid]=dict2["decays"][pid]
+            for pidpair,ssms in dict2["ssmultipliers"].items():
+                if pid in pidpair or -pid in pidpair:
+                    ret["ssmultipliers"][pidpair]=ssms
+        else:
+            ret["masses"][pid]=.5*m + .5*ret["masses"][pid]
     return ret
