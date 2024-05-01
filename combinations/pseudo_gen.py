854c1880ee9bf0e1728d2bb50b41b1ef35238f9d
J-Yellen <jamie.d.yellen@gmail.com>
2024-05-01 19:15:41 +0200

    multiple changes to adopt smodels 3.0.0 standards

--- combinations/pseudo_gen.py
+++ combinations/pseudo_gen.py
@@ -1,14 +1,15 @@
 import os
 import numpy as np
-from typing import Optional
+from typing import Optional, Union
 from pathlib import Path
 from ptools.expResModifier import ExpResModifier
-from smodels.theory.theoryPrediction import theoryPredictionsFor
-from smodels.tools.physicsUnits import GeV, fb
+from smodels.experiment.databaseObj import Database
+from smodels.matching.theoryPrediction import theoryPredictionsFor, TheoryPredictionList, TheoryPrediction
+from smodels.base.physicsUnits import GeV, fb
 from smodels.share.models.mssm import BSMList
 from smodels.share.models.SMparticles import SMList
-from smodels.theory.model import Model
-from smodels.theory.decomposer import decompose
+from smodels.base.model import Model
+from smodels.decomposition.decomposer import decompose
 from multiprocessing import Process, Queue
 
 pmodel_path = Path(__file__).parent / 'pmodels/'
@@ -35,46 +36,9 @@ def get_pseudodata_args(database: str, tag: str, seed: float = None):
     return args
 
 
-def gen_pseudodata(database: str, model: Optional[list[str]] = None, datasetIDs: Optional[list[str]] = None,
-                   analysisIDs: Optional[list[str]] = None, seed: Optional[float] = None,
-                   bootstrap_num: int = 1) -> list[ExpResModifier]:
-    """
-    Generate pseudo Experimental results using "ExpResModifier"
-
-    Args:
-        database (str): SModels Database containing experimental data
-        model (Optional[list[str]], optional): List of models (Topologies) provided to SModelS. Defaults to ['All'].
-        datasetIDs (Optional[list[str]], optional): List of dataset IDs. Defaults to ['All'].
-        analysisIDs (Optional[list[str]], optional):List of Analysis IDs. Defaults to ['All'].
-        seed (float, optional): Random seed. Defaults to None.
-        bootstrap_num (int, optional):  Number of bootstrapped sets. Defaults to 1.
-
-    Returns:
-        list[ExpResModifier]: List of bootstrapped Experimental results under the SM hypothesis
-    """
-
-    if analysisIDs is None:
-        analysisIDs = ['all']
-    if datasetIDs is None:
-        datasetIDs = ['all']
-    if model is None:
-        model = ['all']
-
-    expResArgs = dict(analysisIDs=analysisIDs, datasetIDs=datasetIDs, txnames=model)
-    args = get_pseudodata_args(database, tag='fake', seed=seed)
-    modifier = ExpResModifier(args)
-    modifier.filter(expResArgs=expResArgs)
-    updatedListOfExpRes = []
-    for _ in range(bootstrap_num):
-        listOfExpRes = modifier.removeEmpty(modifier.db.expResultList)
-        updatedListOfExpRes.append(modifier.fakeBackgrounds(listOfExpRes))
-    return updatedListOfExpRes
-
-
-def gen_llr(database: str, slhafile: str, labels: list, model: Optional[list[str]] = None,
-            datasetIDs: Optional[list[str]] = None, analysisIDs: Optional[list[str]] = None,
-            seed: Optional[float] = None, bootstrap_num: int = 1, expected: bool = False,
-            anomaly_mode: bool = True) -> list[dict[str, float]]:
+def gen_llr(database: str, slhafile: str, labels: list, analysisIDs: list[str],
+            model: Optional[list[str]] = None, seed: Optional[float] = None, bootstrap_num: int = 1,
+            expected: bool = False, anomaly_mode: bool = True) -> list[dict[str, float]]:
     """
     Generate pseudo NLLR using "ExpResModifier"
 
@@ -94,71 +58,80 @@ def gen_llr(database: str, slhafile: str, labels: list, model: Optional[list[str
         list: List[Dict[str, float]] of bootstraped NLLR results under the SM hypothesis
     """
 
-    if analysisIDs is None:
-        analysisIDs = ['all']
-    if datasetIDs is None:
-        datasetIDs = ['all']
     if model is None:
         model = ['all']
 
-    expResArgs = dict(analysisIDs=analysisIDs, datasetIDs=datasetIDs, txnames=model)
+    # expResArgs = dict(analysisIDs=analysisIDs, datasetIDs=['all'], txnames=model)
     args = get_pseudodata_args(database, tag='fake', seed=seed)
     modifier = ExpResModifier(args)
-    modifier.filter(expResArgs=expResArgs)
+    modifier.filter()
     llr_dict = []
     for _ in range(bootstrap_num):
         listOfExpRes = modifier.removeEmpty(modifier.db.expResultList)
-        exp_results = modifier.fakeBackgrounds(listOfExpRes)
-        llr_dict.append(get_llr_at_point(slhafile, exp_results, labels, expected, anomaly_mode))
+        pseudo_databse = {'database': modifier.db, 'expResults': modifier.fakeBackgrounds(listOfExpRes)}
+        llr_dict.append(get_llr_at_point(slhafile, analysisIDs, expected,
+                                         anomaly_mode, pseudo_databse=pseudo_databse))
     return llr_dict
 
 
-def get_llr_at_point(slhafile: str, exp_results: list, labels: list, expected: bool = False,
-                     anomaly_mode: bool = True) -> dict[str, float]:
+def get_prediction(pred: Union[TheoryPredictionList, TheoryPrediction], anomaly_mode: bool,
+                   expected: bool, min_val: float = np.finfo(float).tiny) -> float:
+    ratio = np.NaN
+    if anomaly_mode:
+        pred.computeStatistics(expected=False)
+        likelihood = pred.lsm(expected=False)
+        lmax = pred.likelihood(expected=False)
+    else:
+        pred.computeStatistics(expected=expected)
+        likelihood = pred.likelihood(expected=expected)
+        lmax = pred.lmax(expected=expected)
 
-    """
-    Calculate the log likelihood ratio for each SR as specified by model point.
+    if likelihood is not None and lmax is not None:
 
-    Args:
-        slhafile (str): Str path to slha file
-        exp_results (list): List SModels Experimental Results
-        labels (list): List of identifying lables -> {analysisId:}:{dataId}
-        expected (bool, optional): bool expected (True) or observed (False). Defaults to False.
-        anomaly_mode (bool, optional): Run in aomaly mode, L(u=0)/L(u=1). Defaults to True.
+        if likelihood > min_val and lmax > min_val:
+            ratio = -2 * np.log(likelihood / lmax)
 
-    Returns:
-        dict[str, float]: Dict of  NLLR results under the SM hypothesis
-    """
+        elif likelihood <= min_val and lmax <= min_val:
+            ratio = 0.0
 
-    min_val = np.finfo(float).tiny
+    return ratio
+
+
+def get_llr_at_point(slhafile: Union[str, Path], analysisIDs: list[str], expected: bool = False,
+                     anomaly_mode: bool = False, pseudo_databse: Optional[dict[str, Database, list]] = None) -> dict:
+
+    srs_in_ids = any([":" in item for item in analysisIDs])
+    if srs_in_ids:
+        print("Signal regions found in analysis ID's.")
+        print("Searching data ID's for possible matches.")
     model = Model(BSMparticles=BSMList, SMparticles=SMList)
     model.updateParticles(inputFile=slhafile)
-    toplist = decompose(model, 0.005*fb, doCompress=True, doInvisible=True, minmassgap=5.*GeV)
-
-    log_lilekihood_ratio = {}
-    for e_r in exp_results:
-        predictions = theoryPredictionsFor(e_r, toplist, useBestDataset=False, combinedResults=False)
-        if predictions is not None:
-            for pred in predictions:
-                tag = f"{e_r.globalInfo.id}:{pred.dataset.dataInfo.dataId}"
-                if tag not in labels:
-                    continue
-                if anomaly_mode:
-                    pred.computeStatistics(expected=False)
-                    l_Upper = pred.lsm(expected=False)
-                    l_lower = pred.likelihood(expected=False)
-                else:
-                    pred.computeStatistics(expected=expected)
-                    l_Upper = pred.likelihood(expected=expected)
-                    l_lower = pred.lmax(expected=expected)
-                if l_Upper is not None and l_lower is not None:
-                    if l_Upper > min_val and l_lower > min_val:
-                        log_lilekihood_ratio[tag] = -2 * np.log(l_Upper / l_lower)
-                    elif l_Upper <= min_val and l_lower <= min_val:
-                        log_lilekihood_ratio[tag] = 0.0
-                    else:
-                        log_lilekihood_ratio[tag] = np.NaN
-    return log_lilekihood_ratio
+    top_dict = decompose(model, sigmacut=0.005*fb, massCompress=True, invisibleCompress=True, minmassgap=5*GeV)
+    if pseudo_databse is None:
+        dbase = Database("official")
+        expResults = dbase.getExpResults(analysisIDs=['all'], datasetIDs=['all'], dataTypes=['efficiencyMap'])
+    else:
+        dbase = pseudo_databse['database']
+        expResults = pseudo_databse['expResults']
+    allThPredictions = theoryPredictionsFor(dbase, top_dict)
+    llr_dict = {}
+    for expRes, thPreds in zip(expResults, allThPredictions):
+        if not thPreds:
+            continue
+        aid_label = expRes.globalInfo.id
+        if aid_label in analysisIDs:
+            llr = get_prediction(thPreds, anomaly_mode=anomaly_mode, expected=expected)
+            if not np.isnan(llr):
+                llr_dict[aid_label] = llr
+        elif srs_in_ids:
+            for dataset in expRes.datasets:
+                if hasattr(dataset.dataInfo, 'dataId'):
+                    sms_label = f"{expRes.globalInfo.id}:{dataset.dataInfo.dataId}"
+                    if sms_label in analysisIDs:
+                        llr = get_prediction(thPreds, anomaly_mode=anomaly_mode, expected=expected)
+                        if not np.isnan(llr):
+                            llr_dict[sms_label] = llr
+    return llr_dict
 
 
 def split_chunks(num: int, proc: int) -> list[int]:
@@ -181,26 +154,36 @@ def split_chunks(num: int, proc: int) -> list[int]:
     return run_chunks
 
 
-# def strip_dict_list(list_of_dict: list, labels: list) -> NDArray:
-#     """_summary_
+def strip_dict_list(list_of_dict: list, labels: list) -> np.ndarray:
+    """_summary_
 
-#     Args:
-#         list_of_dict (list): _description_
-#         labels (list): _description_
+    Args:
+        list_of_dict (list): _description_
+        labels (list): _description_
 
-#     Returns:
-#         NDArray: _description_
-#     """
-#     ret = np.zeros((len(list_of_dict), len(list_of_dict[0])))
-#     for i, element in enumerate(list_of_dict):
-#         for key, item in element.items():
-#             j = labels.index(key)
-#             ret[i, j] = item
-#     return ret
+    Returns:
+        NDArray: _description_
+    """
+    ret = np.zeros((len(list_of_dict), len(labels)))
+    for i, element in enumerate(list_of_dict):
+        for key, item in element.items():
+            if key in labels:
+                j = labels.index(key)
+                ret[i, j] = item
+    return ret
 
 
-def get_pseudo_llr(slha_loc: str, data_base: str, labels: list, datasetIDs: Optional[list[str]] = None,
-                   analysisIDs: Optional[list[str]] = None, num: int = 1, proc: int = 1) -> list:
+def _llr_worker(args: dict, queue: Queue) -> None:
+    """ Helper function to create queue
+    Args:
+        args (dict): Dictionary of arguments passed to gen_llr
+        queue (Queue): Input Queue
+    """
+    queue.put(gen_llr(**args))
+
+
+def get_pseudo_llr(slha_loc: str, data_base: str, labels: list, analysisIDs: list[str],
+                   num: int = 1, proc: int = 1) -> list:
     """_summary_
 
     Args:
@@ -216,21 +199,8 @@ def get_pseudo_llr(slha_loc: str, data_base: str, labels: list, datasetIDs: Opti
         list: _description_
     """
 
-    if analysisIDs is None:
-        analysisIDs = ['all']
-    if datasetIDs is None:
-        datasetIDs = ['all']
-
-    def _llr_worker(args: dict, queue: Queue) -> None:
-        """ Helper function to create queue
-        Args:
-            args (dict): Dictionary of arguments passed to gen_llr
-            queue (Queue): Input Queue
-        """
-        queue.put(gen_llr(**args))
-
-    args = dict(database=data_base, labels=labels, slhafile=slha_loc, datasetIDs=datasetIDs,
-                analysisIDs=analysisIDs, seed=None, num=num)
+    args = dict(database=data_base, labels=labels, slhafile=slha_loc,
+                analysisIDs=analysisIDs)
     if proc < 2 or num == 1:
         args['seed'] = np.random.randint(0, 1e6)
         return gen_llr(**args)
@@ -238,7 +208,7 @@ def get_pseudo_llr(slha_loc: str, data_base: str, labels: list, datasetIDs: Opti
         jobs = []
         input_queue = Queue()
         for item in split_chunks(num, proc):
-            args['num'] = item
+            # args['num'] = item
             args['seed'] = np.random.randint(0, 1e6)
             p = Process(target=_llr_worker, args=(args, input_queue))
             jobs.append(p)
