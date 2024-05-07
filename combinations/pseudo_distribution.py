ebda834e30979183b38c0d4d122da25f76ff9ab9
J-Yellen <jamie.d.yellen@gmail.com>
2024-05-07 14:19:39 +0200

    change return of get_milti_bset_set, swapping indecies for SModelS labels

--- combinations/pseudo_distribution.py
+++ combinations/pseudo_distribution.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pathfinder as pf
 from multiprocessing import Process, Manager
-from typing import Iterable
+from typing import Iterable, Dict, List
 from numpy.typing import NDArray
 
 
-def get_best_set(binary_acceptance_matrix: NDArray, weights: NDArray, sort_bam=False) -> dict[str, NDArray]:
+def get_best_set(binary_acceptance_matrix: NDArray, weights: NDArray, sort_bam=False) -> Dict[str, NDArray]:
     """
     Get the highest sum weights that can be combined according to the binary acceptance matrix.
 
@@ -15,7 +15,7 @@ def get_best_set(binary_acceptance_matrix: NDArray, weights: NDArray, sort_bam=F
         sort_bam (bool, optional): Sort the binary acceptance matrix by weight (descending). Defaults to False.
 
     Returns:
-        dict[str, NDArray]: Containing the combination path indices and sum of weight sum.
+        Dict[str, NDArray]: Containing the combination path indices and sum of weight sum.
     """
     bam = pf.BinaryAcceptance(binary_acceptance_matrix, weights=weights)
     results = {}
@@ -28,15 +28,19 @@ def get_best_set(binary_acceptance_matrix: NDArray, weights: NDArray, sort_bam=F
     return results
 
 
-def get_bam_weight(over: dict, weight: dict) -> dict:
-    """_summary_
+def get_bam_weight(over: Dict, weight: Dict) -> Dict[str, NDArray, List]:
+    """
+    Construct the binary_acceptance_matrix and weights from the the Dictionary type provided by SModelS.
 
     Args:
-        over (dict): _description_
-        weight (dict): _description_
+        over (Dict): SModelS correlation type dictionary in the form: {'label': [list of combinable labels]}
+        weight (Dict): list of weights to chose combination in the form: {'label': weight (float)}
 
     Returns:
-        dict: _description_
+        Dict:
+            bam (NDArray) -> Binary Acceptance Matrix: an N x N Symmetric matrix that defines the allowable combinations
+            weights (NDArray) -> 1 x N Weights that correspond to the binary acceptance matrix indices
+            labels (List) -> 1 x N list of labels that match the binary acceptance and weight indices
     """
     columns_labels = list(weight.keys())
     bam = np.zeros((len(columns_labels), len(columns_labels)), dtype=bool)
@@ -51,39 +55,23 @@ def get_bam_weight(over: dict, weight: dict) -> dict:
             'labels': [columns_labels[i] for i in order]}
 
 
-def split_list(list_in: list, nunber_of_chuncks: int) -> Iterable[list]:
-    """
-    Yield n number of striped chunks from l
-
-    Args:
-        list_in (list): _description_
-        nunber_of_chuncks (int): _description_
-
-    Returns:
-        Iterable: _description_
-
-    Yields:
-        Iterator[Iterable]: _description_
-    """
-    for i in range(0, nunber_of_chuncks):
-        yield list_in[i::nunber_of_chuncks]
-
-
-def get_milti_bset_set(pseudo_gen_dicts: list[dict]) -> dict[str, float, int]:
-    restut = {}
+def get_milti_bset_set(pseudo_gen_dicts: List[Dict]) -> Dict[str, float, int]:
+    result = {}
     for i, item in enumerate(pseudo_gen_dicts):
         bam_wgths = get_bam_weight(item['bam'], item['weights'])
-        restut[i] = get_best_set(bam_wgths['bam'], bam_wgths['weights'])
-    return restut
+        temp_res = get_best_set(bam_wgths['bam'], bam_wgths['weights'])
+        best_labels = [bam_wgths['labels'][p] for p in temp_res['path']]
+        result[i] = {'best': best_labels, 'weight': temp_res['weight']}
+    return result
 
 
-def best_set_worker(pseudo_gen_dicts: list[dict], run_num: int, return_dict: dict,) -> None:
+def best_set_worker(pseudo_gen_dicts: List[Dict], run_num: int, return_dict: Dict,) -> None:
     for key, item in get_milti_bset_set(pseudo_gen_dicts).items():
         idx = (run_num * len(pseudo_gen_dicts)) + key
         return_dict.update({idx: item})
 
 
-def find_best_sets(pseudo_gen_dicts: list[dict[str, NDArray, list]], num_cor: int = 1) -> dict[dict]:
+def find_best_sets(pseudo_gen_dicts: List[Dict[str, NDArray, List]], num_cor: int = 1) -> Dict[Dict]:
 
     """
     Iterate through 2D array of NLLR values finding the best combination for
@@ -101,6 +89,11 @@ def find_best_sets(pseudo_gen_dicts: list[dict[str, NDArray, list]], num_cor: in
         list of M results, sum of best combination weights from each NLLR set.
 
     """
+
+    def split_list(list_in: List, nunber_of_chuncks: int) -> Iterable[List]:
+        for i in range(0, nunber_of_chuncks):
+            yield list_in[i::nunber_of_chuncks]
+
     if num_cor < 2:
         print(F"Starting job 1. Calculating {len(pseudo_gen_dicts)} best combinations")
         outputdict = get_milti_bset_set(pseudo_gen_dicts)
@@ -116,4 +109,4 @@ def find_best_sets(pseudo_gen_dicts: list[dict[str, NDArray, list]], num_cor: in
             print(F"Starting job {i+1}. Calculating {len(bam_wgts)} best combinations")
         for p in jobs:
             p.join()
-    return dict(sorted(outputdict.items()))
+    return Dict(sorted(outputdict.items()))
