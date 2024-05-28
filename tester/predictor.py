ca88dba3d7414f491a585ac96358a2d37d33aa08
Sahana Narasimha <Sahana.Narasimha@oeaw.ac.at>
2024-05-28 11:00:15 +0200

    removed critic part, put into separate file

--- tester/predictor.py
+++ tester/predictor.py
@@ -44,7 +44,7 @@ class Predictor ( LoggerBase ):
         self.modifier = None
         self.select = select
         self.expected = expected
-        self.rthreshold = 1.38 ## threshold for rmax
+        self.rthreshold = 1.38 ## threshold for rmax #!TAKE IT Out of here, in critic
         if expected:
             from expResModifier import ExpResModifier
             self.modifier = ExpResModifier()
@@ -198,9 +198,7 @@ class Predictor ( LoggerBase ):
     #     return ret
 
     def predict ( self, protomodel : ProtoModel, sigmacut = 0.02*fb,
-                  strategy : str = "aggressive",
-                  keep_predictions : bool = False,
-                  keep_slhafile : bool = False ) -> bool:
+                  strategy : str = "aggressive",keep_predictions : bool = False, keep_slhafile : bool = False ) -> bool:
         """ Compute the predictions and statistical variables, for a
             protomodel.
 
@@ -214,6 +212,7 @@ class Predictor ( LoggerBase ):
         print out its name
         :returns: False, if no combinations could be found, else True
         """
+        
 
         if hasattr ( self, "predictions" ):
             del self.predictions ## make sure we dont accidentally use old preds
@@ -222,53 +221,36 @@ class Predictor ( LoggerBase ):
 
         # Create SLHA file (for running SModelS)
         slhafile = protomodel.createSLHAFile()
-
-        # First run SModelS using all results and considering only the best signal region.
-        # thats the run for the critic
-        ul_critic_preds, llhd_critic_preds = self.runSModelS( slhafile, sigmacut,
-                                                            allpreds=False, ULpreds=True )
-
-        if keep_predictions:
-            self.ul_critic_preds = ul_critic_preds
-            self.llhd_critic_preds = llhd_critic_preds
-        # Extract the relevant prediction information and store in the protomodel:
-        self.updateModelPredictionsWithULPreds(protomodel, ul_critic_preds)
-        self.updateModelPredictionsWithCombinedPreds(protomodel, llhd_critic_preds,
-                                                        cut=0.1, keep_predictions=keep_predictions)
-        # self.log ( "model is excluded? %s" % str(protomodel.excluded) )
-
-        # Compute the maximum allowed (global) mu value given the r-values
-        # stored in protomodel
-        protomodel.mumax = self.getMaxAllowedMu(protomodel)
-
-        # now use all prediction with likelihood values to compute the Z of the model
-        predictions = self.runSModelS( slhafile, sigmacut,
-                                    allpreds=True, ULpreds=False )
+        
+        # now use all prediction with likelihood values to compute the TL of the model
+        predictions = self.runSModelS( slhafile, sigmacut,allpreds=True, ULpreds=False )
 
         if keep_predictions:
             self.predictions = predictions
 
         # Compute significance and store in the model:
         self.computeSignificance( protomodel, predictions, strategy )
-        if protomodel.Z is None:
-            self.log ( f"done with prediction. Could not find combinations (Z={protomodel.Z})" )
+                
+        if protomodel.TL is None:
+            self.log ( f"done with prediction. Could not find combinations (TL={protomodel.TL})" )
             protomodel.delCurrentSLHA()
             return False
         else:
-            self.log ( f"done with prediction. best Z={protomodel.Z:.2f} (muhat={protomodel.muhat:.2f})" )
+            self.log ( f"done with prediction. best TL={protomodel.TL:.2f} (muhat={protomodel.muhat:.2f})" )
 
         protomodel.cleanBestCombo()
 
         #Recompute predictions with higher accuracy for high score models:
         ## FIXME not a good idea! should probably remove!
-        if False and protomodel.Z > 4.1 and protomodel.nevents < 55000:
-            self.log ( f"Z {protomodel.Z:.2f}>2.7, repeat with higher stats!" )
+        if False and protomodel.TL > 4.1 and protomodel.nevents < 55000:
+            self.log ( f"TL {protomodel.TL:.2f}>2.7, repeat with higher stats!" )
             protomodel.nevents = 100000
             protomodel.computeXSecs()
             self.predict(protomodel,sigmacut=sigmacut, strategy= strategy,
                     keep_predictions = keep_predictions )
 
-        if keep_slhafile:                                                                         self.pprint ( f"keeping {protomodel.currentSLHA}, as requested" )
+        if keep_slhafile:
+            self.pprint ( f"keeping {protomodel.currentSLHA}, as requested" )
         else:
             protomodel.delCurrentSLHA()
         # we keep track of the database version, when predicting
@@ -323,11 +305,9 @@ class Predictor ( LoggerBase ):
         predictions = []
         ulpreds = []
 
-        theoryPredictions = theoryPredictionsFor ( self.database, topos,
-                                       useBestDataset=bestDataSet,
-                                       combinedResults=self.do_srcombine )
+        theoryPredictions = theoryPredictionsFor ( self.database, topos, useBestDataset=bestDataSet, combinedResults=self.do_srcombine )
         preds = TheoryPredictionList(theoryPredictions, maxcond)
-
+        
         if preds != None:
             for pred in preds:
                 if pred.dataType() == 'upperLimit':
@@ -383,103 +363,15 @@ class Predictor ( LoggerBase ):
                 txns = ",".join ( set ( map ( str, p.txnames ) ) )
                 print ( f" - {p.analysisId()}:{dataId}: {txns}" )
 
-    def updateModelPredictionsWithULPreds(self, protomodel, ul_critic_preds):
-        """ Extract information from list of theory predictions and store list of dict with r_obs,
-            r_exp and theory prediction(sorted according to decreasing r_obs values) in the protomodel.
-            Also store description about the critic_tp in the protomodel.
-
-            :param ul_critic_preds: theory predictions for UL-type results
-        """
-
-        rvalues = [] #If there are no predictions set rmax and r2 to 0
-        tpList = []
-        for theorypred in ul_critic_preds:
-            r = theorypred.getRValue(expected=False)
-            if r == None:
-                self.pprint ( "I received %s as r. What do I do with this?" % r )
-                r = 23. #TP: Why high r and not 0?
-            rexp = theorypred.getRValue(expected=True)
-            tpList.append( { "robs": r, "rexp": rexp, "tp": theorypred } )
-            rvalues.append(r)
-        while len(rvalues)<2:
-            rvalues.append(0.)
-        rvalues.sort(reverse = True )
-        tpList.sort ( key = lambda x: x['robs'], reverse = True )
-        srs = ", ".join ( [ f"{x:.2f}" for x in rvalues[:3] ] )
-        self.log ( f"top r values before rescaling are: {srs}" )
-        protomodel.ul_type_rvalues = rvalues #Do not include initial zero values
-        # protomodel.excluded = protomodel.rvalues[0] > self.rthreshold #The 0.99 deals with the case rmax = threshold
-        protomodel.ul_type_tpList = tpList[:]
-        critic_description = []
-        for tp in tpList[:3]:
-            rtype = tp['tp'].dataType(short=True)
-            tmp = f"{tp['tp'].analysisId()}({rtype}):{tp['robs']:.2f}"
-            critic_description.append ( tmp )
-        if len(tpList)>3:
-            critic_description.append ( "...")
-        protomodel.critic_description = "Fast critic datasets:" + ",".join ( critic_description )
-
-    def updateModelPredictionsWithCombinedPreds(self, protomodel, llhd_critic_preds, cut, keep_predictions = False):
-        """ Extract information from list of theory predictions and store robs from
-            the most sensitive combination of analyses in the protomodel.
-
-            :param llhd_critic_preds: theory predictions for EM-type results
-            :param cut: theory predictions giving an r_exp below this cut will not enter the combination
-        """
-        from ptools.helpers import experimentalId
-
-        EMpreds = []
-
-        if cut > 0:
-            for tpred in llhd_critic_preds:
-                r_exp = tpred.getRValue(expected = True)
-                if r_exp >= cut:
-                    EMpreds.append(tpred)
-            if keep_predictions:
-                self.llhd_critic_preds = EMpreds
-        else:
-            EMpreds = llhd_critic_preds
-
-        best_comb, weight = self.combiner.getMostSensitiveCombination(llhd_critic_preds)
-
-        if best_comb:
-            tpCombiner = TheoryPredictionsCombiner(best_comb)
-            r = tpCombiner.getRValue(expected=False)
-
-        if r is None:
-            logger.warning ("The computation of the observed r-value of the most sensitive combination gave None.")
-
-        protomodel.llhd_type_rvalue = r
-
-        protomodel.critic_description += "; llhd-based critic combined datasets:" + ",".join( [experimentalId(comb) for comb in best_comb] ) + f"with r={r}"
-
-        return
-
-
-    def getMaxAllowedMu(self, protomodel):
-        """ Compute the maximum (global) signal strength normalization
-            given the predictions.
-        """
-
-        mumax = float("inf")
-        if protomodel.ul_type_rvalues[0] > 0.:
-            #Set mumax slightly below threshold, so the model is never excluded
-            mumax = 0.999*self.rthreshold / protomodel.ul_type_rvalues[0]
-        if 0 < protomodel.llhd_type_rvalue < mumax:
-            mumax = protomodel.llhd_type_rvalue
-
-        return mumax
 
     def computeSignificance(self, protomodel, predictions, strategy):
-        """ compute the K and Z values, and attach them to the protomodel """
+        """ compute the K and TL values, and attach them to the protomodel """
 
-        self.log ( f"now find combo with highest Z given {len(predictions)} predictions" )
+        self.log ( f"now find combo with highest TL given {len(predictions)} predictions" )
         ## find highest observed significance
         #(set mumax just slightly below its value, so muhat is always below)
         mumax = protomodel.mumax
-        bestCombo,Z,llhd,muhat = self.combiner.findHighestSignificance ( predictions, #strategy, (old version)
-                                            expected=False, #mumax = mumax (old version)
-                                            )
+        bestCombo,TL,muhat = self.combiner.findHighestSignificance ( predictions,expected=False )   #, mumax = mumax
         prior = self.combiner.computePrior ( protomodel )
         ## temporary hack: penalize for missing experiment
         missingExpPenalty = self.combiner.penaltyForMissingResults ( predictions )
@@ -495,13 +387,13 @@ class Predictor ( LoggerBase ):
             protomodel.bestCombo = bestCombo
         else:
             protomodel.bestCombo = self.combiner.removeDataFromBestCombo ( bestCombo )
-        protomodel.Z = Z
+        protomodel.TL = TL
 
-        if Z is None: # Z is None when no combination was found
+        if TL is None: # TL is None when no combination was found
             protomodel.K = None
         else:
-            protomodel.K = self.combiner.computeK ( Z, prior )
-        protomodel.llhd = llhd
+            protomodel.K = self.combiner.computeK ( TL, prior )
+        #protomodel.llhd = llhd
         protomodel.muhat = muhat
         protomodel.letters = self.combiner.getLetterCode(protomodel.bestCombo)
         protomodel.description = self.combiner.getComboDescription(protomodel.bestCombo)
