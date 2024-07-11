6183ea9d96179f7a2fcdd7de3af6fddb3e68e093
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-07-11 13:17:54 +0200

    dicts

--- snippets/create100Dicts.sh
+++ snippets/create100Dicts.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
 for i in `seq -f "%03.0f" 100`; do 
-	[ -e dicts/$i.dict ] || {
-		echo $i;
-		./ptools/expResModifier.py -C -d official -o none -s $i; 
-		mv $i.dict dicts/;
+	j="${i}f07";
+	[ -e dicts/$j.dict ] || {
+		echo $j;
+#./ptools/expResModifier.py -C -d official -o none -s ${i}; 
+		./ptools/expResModifier.py -f 0.7 -C -d official -o none -s ${j};
+		mv $j.dict dicts/;
 	};
 done
