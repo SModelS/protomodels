ea24b075e302277ac75f9455f8512ad283aa60c0
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-08-23 12:05:32 +0200

    added cleaning script

---
+++ snippets/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -rf decays.png hiscores.cache hiscores.dict hiscore.slha history*list history.txt index.html Kmin.conf Kold.conf pmodel*dict rawnumbers.html ruler.png texdoc.png TLold.conf logs slurm
