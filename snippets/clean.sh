2bb59f0ae4270e0a389933150828adf9f27f9df8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-10-04 17:56:51 +0200

    clean up more

--- snippets/clean.sh
+++ snippets/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf slurm logs run.dict hiscores.dict Kmin.conf Kold.conf exceptions.log
+rm -rf slurm logs run.dict hiscores.dict Kmin.conf Kold.conf exceptions.log log.txt history.txt hiscores.cache decays.png ruler.png rawnumbers.html texdoc.png index.html TLold.conf
