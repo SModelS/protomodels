828f660f0f4e7ea8f88828230cdb12cc5a5d580b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-08-27 12:53:05 +0200

    pythia typo

--- ptools/refxsecComputer.py
+++ ptools/refxsecComputer.py
@@ -425,7 +425,7 @@ class RefXSecComputer:
         except ImportError as e:
             pass
         originalConfig = f"{codedir}/ptools/pythia8.cfg"
-        temporaryConfig = tempfile.mktemp( prefix=f"pyhtia8_", suffix=".cfg", dir="/tmp/" )
+        temporaryConfig = tempfile.mktemp( prefix=f"pythia8_", suffix=".cfg", dir="/tmp/" )
         with open(originalConfig,'r') as file:
             lines = file.readlines()
         for i,line in enumerate(lines):
