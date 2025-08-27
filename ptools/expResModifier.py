c66376aed30cd956d773c07f3d26a1721f95e9cb
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-08-27 16:43:55 +0200

    drop 9

--- ptools/expResModifier.py
+++ ptools/expResModifier.py
@@ -84,7 +84,7 @@ Fake SM-only database:
 
 Database with a fake signal:
 ----------------------------
-./expResModifier.py -R $RUNDIR -d original.pcl -s signal1 -P pmodel9.dict
+./expResModifier.py -R $RUNDIR -d original.pcl -s signal1 -P pmodel.dict
 
 Playback the modifications described in playback file "db.dict":
 ----------------------------------------------------------------
