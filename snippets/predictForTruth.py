d131a4381a3c7a606b953d142146c99809a375fa
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-08-28 17:18:27 +0200

    snippet

---
+++ snippets/predictForTruth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+""" simple code snippet that runs the true model, computes predictions, 
+    test statistic, etc
+"""
+
+from builder.manipulator import Manipulator
+# from builder.protomodel import ProtoModel
+from tester.predictor import Predictor
+from tester.critic import Critic
+from smodels.experiment.databaseObj import Database
+
+def predictForTruth():
+    signal_model = "./model_stop.dict"
+    dbpath = "./stop1.pcl"
+    print ( f"[predictForTruth] instantiate database {dbpath}" )
+    db = Database ( dbpath )
+    # protomodel = Protomodel()
+    ma = Manipulator( signal_model )
+    ma.M.dbpath = dbpath
+    predictor = Predictor( 0, db, do_srcombine=True )
+    print ( f"[predictForTruth] predict" )
+    critic = Critic ( "critic", db, do_srcombine = True ) 
+    print ( f"[predictForTruth] critic" )
+    cr, response = critic.predict_critic ( ma.M )
+    print ( "critic", cr, response )
+    predictor.predict ( ma, keep_predictions = True, force_computation_K = True )
+    print ( "K", ma.M.K, "TL", ma.M.TL )
+    import sys, IPython; IPython.embed( colors = "neutral" ); sys.exit()
+
+if __name__ == "__main__":
+    predictForTruth()
