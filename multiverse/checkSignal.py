7053440f3e30b3624340849e833ecbb18cd1a75a
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-08-28 13:21:12 +0200

    checker

--- multiverse/checkSignal.py
+++ multiverse/checkSignal.py
@@ -12,11 +12,9 @@ import sys, os
 sys.path.insert(0,"../")
 from smodels.base.physicsUnits import GeV
 
-def analyseUL ( orig_db, new_db, analysis : str ):
+def analyseUL ( orig_db, new_db, analysis : str, txname : str, masses : list ):
     """ analyse this upper limit result """
     dT = ["upperLimit"] 
-    masses = [[500*GeV, 100*GeV],[500*GeV, 100*GeV]]
-    txname = "T2tt"
     orig_er = orig_db.getExpResults ( analysisIDs = [analysis], txnames=[txname],
                                       dataTypes=dT)[0]
     new_er = new_db.getExpResults ( analysisIDs = [analysis], txnames=[txname],
@@ -37,7 +35,13 @@ def checkSignal():
     # ./expResModifier.py -R ./ -d original.pcl -s stop1 -P model_stop.dict
     new_db = Database ( "./stop1.pcl" )
     analysis = "ATLAS-SUSY-2018-08"
-    analyseUL ( orig_db, new_db, analysis )
+    txname = "T2tt"
+    masses = [[500*GeV, 100*GeV],[500*GeV, 100*GeV]]
+    analyseUL ( orig_db, new_db, analysis, txname, masses )
+    masses = [[550*GeV, 120*GeV],[550*GeV, 120*GeV]]
+    analyseUL ( orig_db, new_db, analysis, txname, masses )
+    masses = [[1000*GeV, 400*GeV],[1000*GeV, 400*GeV]]
+    analyseUL ( orig_db, new_db, analysis, txname, masses )
 
 
 
