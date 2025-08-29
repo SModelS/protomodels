849471774bbb1ba0a1836da4f179ecb340bec7f6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-08-29 14:24:21 +0200

    no signal got added

--- multiverse/checkSignal.py
+++ multiverse/checkSignal.py
@@ -30,22 +30,40 @@ def analyseUL ( orig_db, new_db, analysis : str, txname : str, masses : list ):
     print ( f"orig_ul {orig_ul}, new_ul {new_ul}" )
     import sys, IPython; IPython.embed( colors = "neutral" ); sys.exit()
 
-def analyseCombined ( orig_db, new_db, analysis : str ):
+def analyseCombined ( orig_db, new_db, analysis : str, slhafile : str ):
     """ analyse this upper limit result """
+    from smodels.decomposition import decomposer
+    from smodels.base.physicsUnits import fb, GeV, TeV
+    from smodels.matching.theoryPrediction import theoryPredictionsFor
+    from smodels.tools.particlesLoader import load
+    from smodels.share.models.SMparticles import SMList
+    from smodels.base.model import Model
+    BSMList = load()
+    model = Model(BSMparticles=BSMList, SMparticles=SMList)
+    model.updateParticles(inputFile=slhafile,
+                          ignorePromptQNumbers = ['eCharge','colordim','spin'])
+    # Set main options for decomposition
+    sigmacut = 0.001 * fb
+    mingap = 5.*GeV
+
+    # Decompose model
+    topDict = decomposer.decompose(model, sigmacut,
+                                   massCompress=True, invisibleCompress=True,
+                                   minmassgap=mingap)
     #dT = ["upperLimit"] 
     dT = ["all"] 
-    orig_ers = orig_db.getExpResults ( analysisIDs = [analysis], txnames=[txname],
+    orig_ers = orig_db.getExpResults ( analysisIDs = [analysis], txnames=["all"],
                                       dataTypes=dT)
-    if len(orig_ers) == 0:
-        print ( f"no results for {analysis}:{txname}:{dT}" )
-        return
-    orig_er = orig_ers[0]
-    new_er = new_db.getExpResults ( analysisIDs = [analysis], txnames=[txname],
-                                    dataTypes=dT)[0]
-    orig_ul = orig_er.getUpperLimitFor(mass=masses,txname=txname)
-    new_ul = new_er.getUpperLimitFor(mass=masses,txname=txname)
-    print ( analysis, masses, txname )
-    print ( f"orig_ul {orig_ul}, new_ul {new_ul}" )
+    allPredictions = theoryPredictionsFor(orig_db, topDict, combinedResults=True)
+    print ( "original" )
+    for p in allPredictions:
+        print ( p )
+    new_ers = new_db.getExpResults ( analysisIDs = [analysis], txnames=["all"],
+                                    dataTypes=dT)
+    newPredictions = theoryPredictionsFor(new_db, topDict, combinedResults=True)
+    print ( "new" )
+    for p in newPredictions:
+        print ( p )
     import sys, IPython; IPython.embed( colors = "neutral" ); sys.exit()
 
 
@@ -59,7 +77,7 @@ def checkSignal():
     # ./expResModifier.py -R ./ -d original.pcl -s stop1 -P model_stop.dict
     new_db = Database ( "./signal.pcl" )
     analysis = "ATLAS-SUSY-2018-05-ewk"
-    slhafile = "TChiWZ_300_100.slha"
+    slhafile = "TChiWZ_300_50.slha"
     analyseCombined ( orig_db, new_db, analysis, slhafile )
     #txname = "TChiWZ"
     #masses = [[300*GeV, 100*GeV],[300*GeV, 100*GeV]]
