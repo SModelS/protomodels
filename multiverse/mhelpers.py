33330bed84931175e6249ac8e041fef4e6810616
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-09-02 12:21:26 +0200

    refactor

---
+++ multiverse/mhelpers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+""" module to contain helper methods for the multiverse
+"""
+
+def createMyFile ( signal_model : str = "signal_model.dict",
+        dbpath : str = "./signal.pcl", outfile : str = "my.truth",
+        interactive : bool = False ):
+    from builder.manipulator import Manipulator
+    # from builder.protomodel import ProtoModel
+    from tester.predictor import Predictor
+    from tester.critic import Critic
+    from smodels.experiment.databaseObj import Database
+    print ( f"[predictForTruth] instantiate database {dbpath}" )
+    db = Database ( dbpath )
+    # protomodel = Protomodel()
+    ma = Manipulator( signal_model )
+    ma.M.dbpath = dbpath
+    print ( f"[predictForTruth] starting" )
+    predictor = Predictor( 0, db, do_srcombine=True )
+    critic = Critic ( "critic", db, do_srcombine = True ) 
+    cr, response = critic.predict_critic ( ma.M )
+    print ( f"[predictForTruth] critic: {cr}, {response}" )
+    predictor.predict ( ma, keep_predictions = True, force_computation_K = True )
+    print ( f"[predictForTruth] predict K={ma.M.K} TL={ma.M.TL}" )
+    with open ( signal_model, "rt" ) as f:
+        txt = f.read()
+        d = eval( txt )
+        f.close()
+    with open ( outfile, "wt" ) as f:
+        d.update( { "K": ma.M.K, "TL": ma.M.TL, "llhd_critic": ma.M.llhd_critic,
+                    "ul_critic": ma.M.ul_critic })
+        from ptools.helpers import py_dumps
+        ds = py_dumps ( d, indent = 4 )
+        f.write ( ds + "\n" )
+    print ( f"[predictForTruth] wrote truth into my.truth" )
+    if interactive:
+        import sys, IPython; IPython.embed( colors = "neutral" ); sys.exit()
+
--- snippets/predictForTruth.py
+++ snippets/predictForTruth.py
@@ -4,40 +4,9 @@
     test statistic, etc
 """
 
-from builder.manipulator import Manipulator
-# from builder.protomodel import ProtoModel
-from tester.predictor import Predictor
-from tester.critic import Critic
-from smodels.experiment.databaseObj import Database
-
-def predictForTruth ( interactive : bool = False ):
-    signal_model = "./signal_model.dict"
-    dbpath = "./signal.pcl"
-    print ( f"[predictForTruth] instantiate database {dbpath}" )
-    db = Database ( dbpath )
-    # protomodel = Protomodel()
-    ma = Manipulator( signal_model )
-    ma.M.dbpath = dbpath
-    print ( f"[predictForTruth] starting" )
-    predictor = Predictor( 0, db, do_srcombine=True )
-    critic = Critic ( "critic", db, do_srcombine = True ) 
-    cr, response = critic.predict_critic ( ma.M )
-    print ( f"[predictForTruth] critic: {cr}, {response}" )
-    predictor.predict ( ma, keep_predictions = True, force_computation_K = True )
-    print ( f"[predictForTruth] predict K={ma.M.K} TL={ma.M.TL}" )
-    with open ( signal_model, "rt" ) as f:
-        txt = f.read()
-        d = eval( txt )
-        f.close()
-    with open ( "my.truth", "wt" ) as f:
-        d.update( { "K": ma.M.K, "TL": ma.M.TL, "llhd_critic": ma.M.llhd_critic,
-                    "ul_critic": ma.M.ul_critic })
-        from ptools.helpers import py_dumps
-        ds = py_dumps ( d, indent = 4 )
-        f.write ( ds + "\n" )
-    print ( f"[predictForTruth] wrote truth into my.truth" )
-    if interactive:
-        import sys, IPython; IPython.embed( colors = "neutral" ); sys.exit()
+from multiverse.mhelpers import createMyFile
 
 if __name__ == "__main__":
-    predictForTruth( interactive = False )
+    createMyFile ( signal_model = "signal_model.dict",
+        dbpath = "./signal.pcl", outfile = "my.truth",
+        interactive = False )
