2cdcb8a6a8d1143b8672b4a0ec7ac4fe83cb0348
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-09-10 10:47:42 +0200

    dbpath

--- walker/initialiser.py
+++ walker/initialiser.py
@@ -490,7 +490,8 @@ class Initialiser ( LoggerBase ):
     def interact ( self ):
         """ interactive shell, for debugging and development """
         from tester.predictor import Predictor
-        pr = Predictor(0, do_srcombine = True )
+        dbpath = "official"
+        pr = Predictor("ini", dbpath, do_srcombine = True )
         import IPython
         IPython.embed( colors = "neutral" )
 
