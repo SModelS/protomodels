802b11a72687eec337111a172fd0b221213d964e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-10-17 15:16:15 +0200

    dt added

--- snippets/extractModels.py
+++ snippets/extractModels.py
@@ -2,17 +2,23 @@
 
 """ take all models in Pmodels, agglomerate them into a simple list """
 
-
 def extractModels( after : int = 10 ):
-    import glob
+    import glob, sys
+    from datetime import datetime
     files = glob.glob ( "logs/walker_*.log" )
     models = {}
+    startTime, endTime = None, None
     for fl in files:
         with open ( fl, "rt" ) as f:
             lines = f.readlines()
             stepnr = 0
             weDone=False
             for line in lines:
+                if "It is" in line:
+                    p1 = line.find("-")
+                    p2 = line.find("]")
+                    fmt = "%H:%M:%S"
+                    startTime = datetime.strptime(line[p1+1:p2], fmt)
                 if weDone == True:
                     break
                 p2 = line.find(" begins")
@@ -24,8 +30,14 @@ def extractModels( after : int = 10 ):
                 p3 = line.find("Protomodel:")
                 if p3 > 0: #  and stepnr == after:
                     tmp = line[p3+11:]
+                    p1 = line.find("-")
+                    p2 = line.find("]")
+                    fmt = "%H:%M:%S"
+                    endTime = datetime.strptime(line[p1+1:p2], fmt)
                     m = eval ( tmp )
+                    m["dt"] = (endTime - startTime).seconds
                     models[fl]=m
+                    
                 if stepnr == after:
                     weDone = True
                     break
