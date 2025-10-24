0ff5a5f2b9c661790ffa1645c4d8a18eb2559538
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-10-24 15:57:22 +0200

    dont mention dbpath

--- snippets/predictForTruth.py
+++ snippets/predictForTruth.py
@@ -8,5 +8,4 @@ from multiverse.mhelpers import createMyFile
 
 if __name__ == "__main__":
     createMyFile ( signal_model = "signal_model.dict",
-        dbpath = None, outfile = "truth.dict",
-        interactive = False )
+        outfile = "truth.dict", interactive = False )
