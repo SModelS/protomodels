1c01e139332318b96dcaadebd20e8557a0aace3e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-12-29 12:55:46 +0100

    UL critic is plotted

--- helpers/various.py
+++ helpers/various.py
@@ -4,8 +4,12 @@
 
 def viewImage ( imgfile : str ):
     """ view an image on the terminal, if the timg tool exists. """
+    from smodels_utils.plotting.mpkitty import timg
+    timg ( imgfile )
+    """
     import shutil
     if shutil.which ("timg") is not None:
         import subprocess
         o = subprocess.getoutput ( f"timg {imgfile}" )
         print ( o )
+    """
--- plotting/plotNLLs.py
+++ plotting/plotNLLs.py
@@ -4,6 +4,7 @@
 
 import os
 from base.loggerbase import LoggerBase
+import matplotlib.pyplot as plt
 
 class NLLPlotter ( LoggerBase ):
     """ our second generation 2d nll plotter """
@@ -15,6 +16,8 @@ class NLLPlotter ( LoggerBase ):
             self.findInputFile()
         self.readInputFile()
         self.plot()
+        if self.args["show"]:
+            self.show()
         if self.args["interact"]:
             self.interact()
 
@@ -47,8 +50,70 @@ class NLLPlotter ( LoggerBase ):
         with open ( ifile, "rb" ) as f:
             self.data = pickle.load ( f )
 
+    def getCriticList ( self, critic_type : str = "ul" ) -> list:
+        """
+        get the list of critic outputs per point
+        :param critic_type: one of: ul, llhd
+
+        :returns: list of dictionaries, "mx", "my", "passes" as keys
+        """
+        assert critic_type in [ "ul", "llhd" ], \
+             f"critic type should be one of: ul, llhd"
+        ret = []
+        for masspoint in self.data["masspoints"]:
+            mx, my = masspoint["mx"], masspoint["my"]
+            passes = masspoint["critic"][critic_type]["passes"]
+            tmp = { "mx": mx, "my": my, "passes": passes }
+            ret.append ( tmp )
+        return ret
+
     def plot ( self ):
-        self.pprint ( f"plotting {self.args['inputfile']}" )
+        critic_points = self.getCriticList()
+        self.plotBooleanMap ( critic_points )
+        self.pprint ( f"plotting {self.args['inputfile']} -> {self.outputfile}" )
+
+    def plotBooleanMap ( self, points : list[dict] ):
+        """ given a list of dicts, draw the contour
+        :param points: a list of dictionaries, "mx", "my", "passes"
+        """
+        import numpy as np
+        from scipy.interpolate import griddata
+
+        # Extract arrays
+        x = np.array([d["mx"] for d in points])
+        y = np.array([d["my"] for d in points])
+        z = np.array([d["passes"] for d in points], dtype=float)
+
+        # Create interpolation grid
+        xi = np.linspace(x.min(), x.max(), 200)
+        yi = np.linspace(y.min(), y.max(), 200)
+        Xi, Yi = np.meshgrid(xi, yi)
+
+        # Interpolate boolean field
+        Zi = griddata((x, y), z, (Xi, Yi), method="linear")
+
+        # Draw contour where passes == True
+        plt.contour(Xi, Yi, Zi, levels=[0.5])
+        plt.scatter(x, y, c=z, cmap="coolwarm", s=30)
+        plt.xlabel("mx")
+        plt.ylabel("my")
+        plt.title("Contour of passes == True")
+        self.savefig()
+
+    def savefig ( self ):
+        """ save the figure to file """
+        figname = self.args["outputfile"]
+        if "@@I@@" in figname:
+            from pathlib import Path
+            ifile = Path ( self.args["inputfile"] ).stem
+            figname = figname.replace("@@I@@",ifile)
+        self.outputfile = figname
+        # self.pprint ( f"saving to {figname}" )
+        plt.savefig ( figname )
+
+    def show ( self ):
+        from smodels_utils.plotting.mpkitty import timg
+        timg ( self.outputfile )
 
     def interact ( self ):
         import sys, IPython; IPython.embed( colors = "neutral" )
@@ -61,7 +126,12 @@ if __name__ == "__main__":
     argparser.add_argument ( '-i', '--inputfile',
             help='input file',
             type=str, default=None )
+    argparser.add_argument ( '-o', '--outputfile',
+            help='output file [@@I@@.png]',
+            type=str, default="@@I@@.png" )
     argparser.add_argument ( '-I', '--interact',
             help='start interactive shell', action="store_true" )
+    argparser.add_argument ( '-s', '--show',
+            help='show image', action="store_true" )
     args = argparser.parse_args()
     plotter = NLLPlotter ( vars(args) )
