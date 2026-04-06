dc418739039d56ac5fc277a445c11e2db1066489
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-04-06 21:58:16 +0200

    common frontend

--- multiverse/randomNumbers.py
+++ multiverse/randomNumbers.py
@@ -32,15 +32,35 @@ def poisson_parallel(n, lam):
         out[i] = np.random.poisson(lam)
     return out
 
+def fast_rvs(dist, n, **p):
+    if n < 100000:
+        import scipy.stats
+        if dist == "normal":
+            return scipy.stats.norm.rvs ( [p.get("loc",0.0)]*n,
+                [p.get("scale",1.0)]*n )
+        if dist == "lognorm":
+            return scipy.stats.lognorm.rvs ( s=[p.get("s",0.0)]*n,
+                scale=[p.get("scale",1.0)]*n,loc=[p.get("loc",0.0)]*n )
+        if dist == "poisson":
+            return scipy.stats.poisson.rvs ( [p.get("mu",0.0)]*n )
+            
+    if dist == "normal":
+            return normal_parallel(n, p.get("loc",0.0), p.get("scale",1.0))
+    if dist == "lognorm":
+        return lognorm_parallel(n, p["s"], p.get("scale",1.0), p.get("loc",0.0))
+    if dist == "poisson":
+        return poisson_parallel(n, p["mu"])
+
 if __name__ == "__main__":
     import time
     import scipy.stats
-    x = lognorm_parallel ( 1, 1., 1., 0. )
-    n = 10_000_000
+    x = fast_rvs ( "lognorm", 1, s=1., scale=1., loc=0. )
+    n = 100_000_000
+    # n = 100000
     t0 = time.time()
-    x = lognorm_parallel ( n, s=1., scale=1., loc=0. )
+    x = fast_rvs ( "lognorm", n, s=1., scale=1., loc=0. )
     t1 = time.time()
-    print ( np.mean(x), np.std(x), t1-t0 )
+    print ( "numba", np.mean(x), np.std(x), t1-t0 )
     x = scipy.stats.lognorm.rvs ( s=[1.]*n, scale=[1.]*n, loc=[0.]*n )
     t2 = time.time()
-    print ( np.mean(x), np.std(x), t2-t1 )
+    print ( "scipy", np.mean(x), np.std(x), t2-t1 )
