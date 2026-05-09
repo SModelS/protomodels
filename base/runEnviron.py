0dbe02a3704f76b702226299844fe8bebddc1e65
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-05-09 22:33:18 +0200

    create environ when calling

--- base/runEnviron.py
+++ base/runEnviron.py
@@ -224,6 +224,6 @@ class RunEnviron ( LoggerBase ):
             setattr ( self, key, value )
 
 if __name__ == "__main__":
-    #environ = RunEnviron()
+    environ = RunEnviron.create()
     #print ( "environment at environ. Try e.g. environ.templateSLHA" )
     import sys, IPython; IPython.embed( colors = "neutral" ); sys.exit()
