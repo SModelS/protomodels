112a9ce9f669c5992273f173aa1202ac522b4254
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-06-25 12:01:40 +0200

    updated 312fast dict

--- multiverse/expResModifier.py
+++ multiverse/expResModifier.py
@@ -1601,7 +1601,7 @@ Just filter the database:
             t1 = time.time()
             dt = t1 - t0
             if dt > 3:
-                self.pprint ( f"{expRes.globalInfo.id} took {t1-t0:.2f}s" )
+                self.pprint ( f"\n{expRes.globalInfo.id} took {t1-t0:.2f}s" )
         self.log ( "done faking the backgrounds" )
         return ret
 
